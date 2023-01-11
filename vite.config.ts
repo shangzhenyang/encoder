dbd55b4447f411282eafb72d57260b9962e7ef6c
Shangzhen Yang <shangzheny01@gmail.com>
2023-01-10 16:52:27 -0800

    Add autoprefixer

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import autoprefixer from "autoprefixer";
 import eslint from "@rollup/plugin-eslint";
 import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	css: {
+		postcss: {
+			plugins: [autoprefixer]
+		}
+	},
 	plugins: [
 		react(),
 		{

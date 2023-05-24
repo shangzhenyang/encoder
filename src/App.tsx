35b5754092766eb521ee3fac83b8830b5690f358
Shangzhen Yang <shangzheny01@gmail.com>
2023-05-24 15:51:06 -0700

    Change browser update url

--- src/App.tsx
+++ src/App.tsx
@@ -280,7 +280,7 @@ function App() {
 			reminder: 0,
 			reminderClosed: 0,
 			unsupported: true,
-			url: "https://www.google.cn/chrome/"
+			url: "https://browsehappy.com/"
 		});
 		setTimeout(() => {
 			ReactGA.initialize("G-H0PC8ZZ7BN");
